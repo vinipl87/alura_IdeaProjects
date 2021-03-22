0c91a167831c33e137fa9a8e5d5050d7c9ba52ff
Vinícius Lazzeris <vinipl87@gmail.com>
2021-03-22 19:53:36 -0300

    initial commit

--- bytebank/src/br/com/alura/bytebank/exception/FalhaAutenticacaoException.kt
+++
@@ -1,5 +0,0 @@
-package br.com.alura.bytebank.exception
-
-class FalhaAutenticacaoException(
-    mensagem: String = "Falha na autenticação"
-) : Exception(mensagem)
--- bytebank/src/br/com/alura/bytebank/exception/SaldoInsuficienteException.kt
+++
@@ -1,5 +0,0 @@
-package br.com.alura.bytebank.exception
-
-class SaldoInsuficienteException(
-    mensagem: String = "O saldo é insuficiente"
-) : Exception(mensagem)
